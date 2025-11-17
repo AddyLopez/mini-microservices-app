1c66a7af117e19b95e385592396ed6fde166f573
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-17 14:41:28 -0700

    Add event handling to CommentCreate

--- client/src/components/Comments/CommentCreate.js
+++ client/src/components/Comments/CommentCreate.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 // Comments are associated with a particular post's ID
 const CommentCreate = ({ postId }) => {
+  const [content, setContent] = useState("");
+
+  const handleChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Create new comment and associate it with the given post. Include content in post request body.
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content,
+    });
+
+    setContent(""); // Reset input box
+  };
+
   return (
     <section>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label>New Comment</label>
-          <input className="form-control" />
+          <input
+            className="form-control"
+            value={content}
+            onChange={handleChange}
+          />
         </div>
         <button className="btn btn-primary">Submit</button>
       </form>
