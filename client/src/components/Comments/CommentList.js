18e5df6fa96c35464a6e569c123dc668ac73d2b1
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-17 14:54:27 -0700

    Fetch and render comments list

---
+++ client/src/components/Comments/CommentList.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const CommentList = ({ postId }) => {
+  const [comments, setComments] = useState([]); // API sends back an array of comments associated with given post
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      `http://localhost:4001/${postId}/comments`
+    );
+    const data = response.data;
+
+    setComments(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []); // Call this function only once, upon loading
+
+  // Map over array of comments and return its contents
+  const renderedComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
+
+export default CommentList;
