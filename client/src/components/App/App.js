f94a854d60a2bb419ce5207d50ea7f372c9f3e33
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-17 14:02:20 -0700

    Set up PostList component

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import PostCreate from "../Posts/PostCreate/PostCreate";
+import PostList from "../Posts/PostList/PostList";
 
 function App() {
   return (
     <div className="App container">
       <h1>Create Post</h1>
       <PostCreate />
+      <hr />
+      <h2>Posts</h2>
+      <PostList />
     </div>
   );
 }
---
+++ client/src/components/Posts/PostList/PostList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PostList = () => {
+  return (
+    <section className="PostList">
+      <h2>PostList</h2>
+    </section>
+  );
+};
+
+export default PostList;
