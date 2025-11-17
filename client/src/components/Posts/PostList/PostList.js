fb1c1bbdd7fa952613ee34f5a41e3c432b9e2a94
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-17 14:10:49 -0700

    Fetch list of posts

--- client/src/components/Posts/PostList/PostList.js
+++ client/src/components/Posts/PostList/PostList.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const PostList = () => {
+  const [posts, setPosts] = useState({}); // Post service returns an object with a nested array
+
+  const fetchPosts = async () => {
+    const response = await axios.get("http://localhost:4000/posts");
+    const data = response.data;
+
+    setPosts(data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []); // Run this function only once, upon loading
+
+  console.log(posts);
+
   return (
     <section className="PostList">
       <h2>PostList</h2>
