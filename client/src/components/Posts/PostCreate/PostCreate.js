c6ac8e74252a4c9c6b1262f6cefd533bda135e9a
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-17 13:49:45 -0700

    Build POST submission with event handling

--- client/src/components/Posts/PostCreate/PostCreate.js
+++ client/src/components/Posts/PostCreate/PostCreate.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const PostCreate = () => {
+  const [title, setTitle] = useState("");
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Make async post request to posts service on port 4000, send title in body of request
+    await axios.post("http://localhost:4000/posts", {
+      title,
+    });
+
+    setTitle(""); // Reset input box once successful
+  };
+
   return (
     <section className="PostCreate">
-      <form>
+      <form onSubmit={handleSubmit}>
         <section className="form-group">
           <label>Title</label>
-          <input className="form-control" />
+          <input
+            value={title}
+            onChange={handleChange}
+            className="form-control"
+          />
         </section>
         <button className="btn btn-primary">Submit</button>
       </form>
