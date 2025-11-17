112d11b0875969aa04915537d599b2518a5cdd7f
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-17 12:45:56 -0700

    Add POST route handling with annotations

--- comments/index.js
+++ comments/index.js
@@ -5,9 +5,24 @@ const { randomBytes } = require("crypto");
 const app = express();
 app.use(bodyParser.json());
 
+const commentsByPostId = {};
+
+// Get array of comments associated with particular post's ID
 app.get("posts/:id/comments", (req, res) => {});
 
-app.post("posts/:id/comments", (req, res) => {});
+app.post("posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex"); // Generate random ID for comment
+  // Expected request body {content: string}
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || []; // Check if there's already an array for the given id. If undefined, return new empty array.
+  comments.push({ id: commentId, content }); // Push new comment into comments array
+
+  // Assign comments array back to given post inside commentsByPostId object to update it with new content
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments); // Send back entire array of comments
+});
 
 app.listen(4001, () => {
   console.log("Listening on port 4001");
