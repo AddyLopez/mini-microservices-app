2575c6d6c54eaedc927b2e3e955b9c1c6746a0a9
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-17 11:27:56 -0700

    Configure POST route handler and bodyParser

--- posts/index.js
+++ posts/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.json());
 
 const posts = {}; // Store every post created
 
@@ -7,7 +11,17 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {});
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex"); // Generate random id
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(posts[id]);
+});
 
 app.listen("4000", () => {
   console.log("Listening on port 4000");
