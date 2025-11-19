36aa756c8aa32e1c64df85a11502cd45af6ced15
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-19 16:41:18 -0700

    Set up route handlers and port

--- query/index.js
+++ query/index.js
@@ -5,3 +5,13 @@ const cors = require("cors");
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
+// Get list of posts
+app.get("/posts", (req, res) => {});
+
+// Route receives events from event bus
+app.post("/events", (req, res) => {});
+
+app.listen(4002, () => {
+  console.log("Listening on Port 4002...");
+});
