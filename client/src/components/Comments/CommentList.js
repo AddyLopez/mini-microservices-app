37e6a79f2c44ee47743941ee4319af7e8ec14ae8
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-19 22:15:38 -0700

    Update UI based on comment moderation

--- client/src/components/Comments/CommentList.js
+++ client/src/components/Comments/CommentList.js
@@ -3,6 +3,19 @@ import React from "react";
 const CommentList = ({ comments }) => {
   // Map over array of comments and return its contents
   const renderedComments = comments.map((comment) => {
+    let content;
+
+    // Update UI based on comment moderation
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation.";
+    }
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected.";
+    }
+
     return <li key={comment.id}>{comment.content}</li>;
   });
 
