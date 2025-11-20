99649b3f0a44a56e5406fade5cb5cf86be456b9c
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-19 17:03:00 -0700

    Complete post route to receive events from bus

--- query/index.js
+++ query/index.js
@@ -6,11 +6,31 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const posts = {};
+
 // Get list of posts
 app.get("/posts", (req, res) => {});
 
 // Route receives events from event bus
-app.post("/events", (req, res) => {});
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+
+    // Add comment to comments array of a given post
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  // Indicate by response empty object that event was received and processed
+  res.send({});
+});
 
 app.listen(4002, () => {
   console.log("Listening on Port 4002...");
