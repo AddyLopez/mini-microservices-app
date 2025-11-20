4f1fcf2cb64794d1c33cf77b84c8569321de15a4
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-11-19 20:39:12 -0700

    Add comment moderation logic

--- moderation/index.js
+++ moderation/index.js
@@ -5,9 +5,34 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
-// Purpose of this service is to watch for CommentCreated event(s) and emit CommentModerated event
+// Purpose of this service is to watch for CommentCreated event(s) and emit CommentModerated event(s)
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
 
-app.post("/events", (req, res) => {});
+  const flaggedWord = "orange"; // Arbitrary word to flag to moderate comments
+
+  if (type === "CommentCreated") {
+    // Decide to approve or reject comment with ternary expression
+    const status = data.content.includes(flaggedWord) ? "rejected" : "approved";
+
+    // Emit CommentModerated event to event bus with updated status
+    await axios
+      .post("http://localhost:4005/events", {
+        type: "CommentModerated",
+        data: {
+          id: data.id,
+          postId: data.postId,
+          status,
+          content: data.content,
+        },
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+  // Send response so that request handler won't hang
+  res.send({});
+});
 
 app.listen(4003, () => {
   console.log("Listening on Port 4003...");
